9d6a48ef9f46b47665269c1d8e094f4349e35123
JaviPardox <javiyupipa@gmail.com>
2024-09-30 21:39:41 +0200

    Dropped margin on small screens

--- frontend/src/components/ResponsiveCenterLayout.tsx
+++ frontend/src/components/ResponsiveCenterLayout.tsx
@@ -4,7 +4,7 @@ const ResponsiveCenterLayout: React.FC<{ children: React.ReactNode }> = ({ child
   return (
     <div className="bg-openai-dark text-openai-light min-h-screen flex justify-center">
         {/* div className="ring-1 ring-zinc-300/20 fixed inset-0 bg-openai-center-content z-0 max-w-[76rem] mx-auto"></div> */}
-      <div className="ring-1 ring-zinc-300/20 bg-openai-center-content w-full max-w-[76rem] min-w-[20rem] mx-8 relative z-10">
+      <div className="ring-1 ring-zinc-300/20 bg-openai-center-content w-full max-w-[76rem] min-w-[20rem] sm:mx-8 relative z-10">
         <div className="px-4 sm:px-6 lg:px-8">
           {children}
         </div>
