96ec66980ee0d23a74de6df5fd41ef8502b6024d
JaviPardox <javiyupipa@gmail.com>
2024-10-09 18:41:28 +0200

    Properly typed the component

--- frontend/src/components/WhatIOfferList.tsx
+++ frontend/src/components/WhatIOfferList.tsx
@@ -1,7 +1,8 @@
+import React, { useState } from 'react';
 import WhatIOfferListItem from './WhatIOfferListItem';
 import { FaTools, FaCode, FaFire, FaChartLine, FaCommentAlt, FaLightbulb } from 'react-icons/fa';
 
-const WhatIOfferList = () => {
+const WhatIOfferList: React.FC = () => {
   return (
     <ul className="grid grid-cols-1 gap-x-12 gap-y-12 md:gap-y-16 sm:grid-cols-2 lg:grid-cols-3 text-zinc-400 mt-10">
       <WhatIOfferListItem
