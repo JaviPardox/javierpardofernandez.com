b433ca40314c1c38a140ade9adb46307fbcf696e
JaviPardox <javiyupipa@gmail.com>
2024-10-11 14:39:05 +0200

    Added types for experience

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -3,3 +3,22 @@ export interface Project {
     title: string;
     description: string;
   }
+
+export interface IconItem {
+  content: string;
+  iconClass: string;
+}
+
+export interface CompanyAndDateInfo {
+  url: string;
+  companyName: string;
+  date: string;
+}
+
+export interface WorkExperienceItem {
+  hashtags: string[];
+  icons: IconItem[];
+  companyAndDateInfo: CompanyAndDateInfo;
+  jobPosition: string;
+  jobDescription: string[];
+}
