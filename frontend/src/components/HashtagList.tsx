c1803d57a1052f2cd26981849ea3a661b9c7eb9c
JaviPardox <javiyupipa@gmail.com>
2024-10-15 14:08:32 +0200

    Changed text color to zinc

--- frontend/src/components/HashtagList.tsx
+++ frontend/src/components/HashtagList.tsx
@@ -7,7 +7,7 @@ interface HashtagListProps {
 
 const HashtagList: React.FC<HashtagListProps> = ({ items }) => {
   return (
-    <ul className="mt-6 gap-1 md:gap-2 flex flex-wrap">
+    <ul className="mt-6 gap-1 md:gap-2 flex flex-wrap text-zinc-300">
       {items.map((item, index) => (
         <HashtagListItem key={index} item={item} />
       ))}
