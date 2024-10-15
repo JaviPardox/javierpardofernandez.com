332401125304f1076b263bf0d98f4eb66dcdfaff
JaviPardox <javiyupipa@gmail.com>
2024-10-15 14:12:51 +0200

    Implemented react tooltips, added fading, and styling

--- frontend/src/components/DevIcon.tsx
+++ frontend/src/components/DevIcon.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Tooltip as ReactTooltip } from 'react-tooltip';
+
 
 interface IconProps {
   content: string;
@@ -6,12 +8,27 @@ interface IconProps {
   isHovered: boolean;
 }
 
+
 const DevIcon: React.FC<IconProps> = ({ content, iconClass, isHovered }) => {
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+
   return (
-    <i
-      data-tooltip-content={content}
-      className={`${iconClass} transition-colors ${isHovered ? 'colored' : ''}`}
-    ></i>
+    <>
+      <i
+        data-tooltip-id={`tooltip-${content}`}  // Use data-tooltip-id
+        className={`${iconClass} transition-colors ${isHovered ? 'colored' : ''}`}
+        onMouseEnter={() => setIsTooltipVisible(true)}
+        onMouseLeave={() => setIsTooltipVisible(false)}
+      ></i>
+      <ReactTooltip
+        id={`tooltip-${content}`}  // Set the tooltip id to match data-tooltip-id
+        content={content}
+        place="top"
+        className={`custom-tooltip ${isTooltipVisible ? 'show' : ''}`}
+        classNameArrow="custom-tooltip-arrow"
+        delayHide={300} 
+      />
+    </>
   );
 };
 
