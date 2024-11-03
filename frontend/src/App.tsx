42e7fbdb807ab9facdca188e770dd4d0b66848db
JaviPardox <javiyupipa@gmail.com>
2024-11-03 19:36:27 +0100

    Added fade in and fade out animations between pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import Navbar from './components/Navbar';
 import ResponsiveCenterLayout from './components/ResponsiveCenterLayout';
-import Home from './pages/Home';
-import Blog from './pages/Blog';
 import PageTransitions from './components/PageTransitions';
+import PageFadeTransitions from './components/PageFadeTransitions';
 
 
 const App: React.FC = () => {
@@ -18,10 +17,7 @@ const App: React.FC = () => {
           <Navbar />  
             <ResponsiveCenterLayout>
               <main className="p-4 sm:p-6 lg:p-8">
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/blog" element={<Blog />} />
-                </Routes>
+                <PageFadeTransitions />
               </main>
             </ResponsiveCenterLayout>
           </PageTransitions>
