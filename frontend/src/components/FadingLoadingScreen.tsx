6aeb45042ebac33cffda583fb6c8f5da26d8358f
JaviPardox <javiyupipa@gmail.com>
2024-11-05 11:13:17 +0100

    Finished loading screen

--- frontend/src/components/FadingLoadingScreen.tsx
+++ frontend/src/components/FadingLoadingScreen.tsx
@@ -1,13 +1,126 @@
-import React from 'react';
-import { PulseLoader, ClipLoader, BeatLoader, RingLoader, HashLoader } from 'react-spinners';
+import React from "react";
 
 const FadingLoadingScreen = () => {
   return (
     <div className="relative flex flex-col items-center gap-4 animate-contentFadeIn">
-      <div className="flex flex-col items-center gap-2">
-        <p className="text-2xl font-light text-zinc">Welcome</p>
+      <div className="measure"></div>
+      <div id="colorWrapper" className="phosGreen-II">
+        <div className="grid">
+          <div className="cube">
+            <div className="lifter">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="cube">
+            <div className="lifter">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="cube">
+            <div className="lifter">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="cube">
+            <div className="lifter">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="cube">
+            <div className="lifter">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="cube">
+            <div className="lifter">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="cube">
+            <div className="lifter">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="cube">
+            <div className="lifter">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="cube">
+            <div className="lifter">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="cube">
+            <div className="lifter">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="cube">
+            <div className="lifter">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="cube">
+            <div className="lifter">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="cube">
+            <div className="lifter">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="cube">
+            <div className="lifter">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="cube">
+            <div className="lifter">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+          <div className="cube">
+            <div className="lifter">
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+              <div className="cube__face"></div>
+            </div>
+          </div>
+        </div>
       </div>
-      <HashLoader color="#14b8a6" size={65} />
+      <div className="shadow flatShadow dropShadow"> </div>
     </div>
   );
 };
