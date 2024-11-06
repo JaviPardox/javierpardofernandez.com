7e32c3a51563f063038b6fe68ee2e69e59bd83d9
JaviPardox <javiyupipa@gmail.com>
2024-11-06 13:05:37 +0100

    Comented out custom hook for now

--- frontend/src/hooks/usePageLoading.ts
+++ frontend/src/hooks/usePageLoading.ts
@@ -1,22 +1,22 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useAppDispatch } from '../store/hooks';
-import { setLoading } from '../store/loadingSlice';
+//import { setLoading } from '../store/loadingSlice';
 
 export const usePageLoading = (loadingTime: number = 1500) => {
   const dispatch = useAppDispatch();
   const location = useLocation();
 
   useEffect(() => {
-    dispatch(setLoading(true));
+    //dispatch(setLoading(true));
 
     const timer = setTimeout(() => {
-      dispatch(setLoading(false));
+      //dispatch(setLoading(false));
     }, loadingTime);
 
     return () => {
       clearTimeout(timer);
-      dispatch(setLoading(false));
+      //dispatch(setLoading(false));
     };
   }, [location.pathname, dispatch, loadingTime]);
 };
