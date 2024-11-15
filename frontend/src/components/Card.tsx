f12d38329a2add3f9adb8ec1e4ca6909c4aaed84
JaviPardox <javiyupipa@gmail.com>
2024-11-15 23:31:30 +0100

    Cards are now bigger in small screens

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ imageId, rotation = 0 }) => {
   return (
     <div
-      className={`relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-800 sm:w-72 sm:rounded-2xl`}
+      className={`relative aspect-[9/10] w-60 flex-none overflow-hidden rounded-xl bg-zinc-800 sm:w-72 sm:rounded-2xl`}
       style={{ transform: `rotate(${rotation}deg)` }}
     >
       <ImageCard imageId={imageId} />
