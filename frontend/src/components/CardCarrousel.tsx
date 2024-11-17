a5c518868ed6bbe22f846696d852e3743937139f
JaviPardox <javiyupipa@gmail.com>
2024-11-17 16:23:26 +0100

    Added snapping effect on card scrolling for small screen - Resolves #36

--- frontend/src/components/CardCarrousel.tsx
+++ frontend/src/components/CardCarrousel.tsx
@@ -14,21 +14,21 @@ const CardCarrousel: React.FC = () => {
 
   return (
     <div className="relative -mx-6 sm:mx-0">
-      <div className="-my-4 flex overflow-x-auto scrollbar-hide gap-5 py-4 sm:overflow-x-visible sm:justify-center sm:gap-8">
+      <div className="-my-4 flex overflow-x-auto scrollbar-hide gap-5 py-4 sm:overflow-x-visible sm:justify-center sm:gap-8 snap-x snap-mandatory">
         <div className="pl-6 sm:pl-0 flex gap-5 sm:gap-8 lg:ml-[25px] ml-0">
-          <div className="card-item">
+          <div className="card-item snap-center">
               <Card rotation={2} imageId="avatar" />
             </div>
-            <div className="card-item">
+            <div className="card-item snap-center">
               <Card rotation={-2} imageId="card1" />
             </div>
-            <div className="card-item">
+            <div className="card-item snap-center">
               <Card rotation={2} imageId="card2" />
             </div>
-            <div className="card-item">
+            <div className="card-item snap-center">
               <Card rotation={2} imageId="card3" />
             </div>
-            <div className="card-item">
+            <div className="card-item snap-center">
               <Card rotation={-2} imageId="card4" />
             </div>
         </div>
