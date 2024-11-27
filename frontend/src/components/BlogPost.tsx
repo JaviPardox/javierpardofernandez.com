db7b05910e1852a6119e37e5d4a9b5272b6783c3
JaviPardox <javiyupipa@gmail.com>
2024-11-27 13:55:20 +0100

    Alligned button with date in small screens

--- frontend/src/components/BlogPost.tsx
+++ frontend/src/components/BlogPost.tsx
@@ -116,7 +116,7 @@ const BlogPost: React.FC = () => {
                 onClick={handleClick}
                 type="button"
                 aria-label="Go back to articles"
-                className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
+                className="group mb-2 sm:mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
               >
                 <svg
                   viewBox="0 0 16 16"
