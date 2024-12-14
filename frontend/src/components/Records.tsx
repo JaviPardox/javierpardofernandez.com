9e6e7c6e35a4fc97a4ba83bad6b2d78c16a5f69c
JaviPardox <javiyupipa@gmail.com>
2024-12-14 13:17:10 +0100

    Advanced work on the section

--- frontend/src/components/Records.tsx
+++ frontend/src/components/Records.tsx
@@ -51,25 +51,53 @@ const RecordsSection: React.FC = () => {
       <div className="lg:w-1/2">
         <h2 className="relative text-4xl mb-10 mt-7 text-zinc-100 tracking-tight leading-[3.5rem] code-themed break-words overflow-x-auto">
           <span className="function-name">$</span>{" "}
-          <span className="string">ls</span>{" "}
-          <span className="keyword">-l</span>{" | "}
+          <span className="string">ls</span> <span className="keyword">-l</span>
+          {" | "}
           <span className="function-name">grep</span>{" "}
           <span className="string">academics</span>
           <span className="cursor"></span>
         </h2>
-        <ul className="space-y-4">
-          {academicRecords.map((record, index) => (
-            <li key={index} className="bg-zinc-100 p-4 rounded-lg shadow">
-              <h3 className="text-lg font-semibold">{record.institution}</h3>
-              <p>
-                {record.degree} in {record.field_of_study}
-              </p>
-              <p>
-                {record.start_date} - {record.end_date}
-              </p>
-            </li>
-          ))}
-        </ul>
+        <div className="rounded-2xl border p-6 border-zinc-700/40">
+          <ul className="space-y-4">
+            {academicRecords.map((record, index) => (
+              <li key={index} className="flex gap-4">
+                <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 border border-zinc-700/50 bg-zinc-800 ring-0">
+                  <img
+                    alt=""
+                    loading="lazy"
+                    width="32"
+                    height="32"
+                    decoding="async"
+                    data-nimg="1"
+                    className="h-7 w-7 rounded-full"
+                    src={`/img/records/${record.logo_path}.png`}
+                    style={{ color: "transparent" }}
+                  ></img>
+                </div>
+                <dl className="flex flex-auto flex-wrap gap-x-2">
+                  <dd className="w-full flex-none text-sm font-medium text-zinc-100">
+                    <span className="italic font-normal text-zinc-100/60">
+                      {record.degree}
+                    </span>
+                    <span className="mx-1">|</span>
+                    {record.field_of_study}
+                  </dd>
+                  <dd className="text-xs text-zinc-400">
+                    {record.institution}
+                  </dd>
+                  <dd
+                    className="ml-auto text-xs text-zinc-500"
+                    aria-label="2019 until Present"
+                  >
+                    <time>{record.start_date}</time>{" "}
+                    <span aria-hidden="true">—</span>{" "}
+                    <time>{record.end_date}</time>
+                  </dd>
+                </dl>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <div className="lg:w-1/2">
         <h2 className="relative text-4xl mb-10 mt-7 text-zinc-100 tracking-tight leading-[3.5rem] code-themed break-words overflow-x-auto">
@@ -77,18 +105,46 @@ const RecordsSection: React.FC = () => {
           <span className="string">"organizations.h"</span>
           <span className="cursor"></span>
         </h2>
-
-        <ul className="space-y-4">
-          {organizations.map((org, index) => (
-            <li key={index} className="bg-zinc-100 p-4 rounded-lg shadow">
-              <h3 className="text-lg font-semibold">{org.name}</h3>
-              <p>
-                {org.role} ({org.duration})
-              </p>
-              <p>{org.description}</p>
-            </li>
-          ))}
-        </ul>
+        <div className="rounded-2xl border p-6 border-zinc-700/40">
+          <ul className="space-y-4">
+            {organizations.map((org, index) => (
+              <li key={index} className="flex gap-4">
+                <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 border border-zinc-700/50 bg-zinc-800 ring-0">
+                  <img
+                    alt=""
+                    loading="lazy"
+                    width="32"
+                    height="32"
+                    decoding="async"
+                    data-nimg="1"
+                    className="h-7 w-7 rounded-full"
+                    src={`/img/records/${org.logo_path}.png`}
+                    style={{ color: "transparent" }}
+                  ></img>
+                </div>
+                <dl className="flex flex-auto flex-wrap gap-x-2">
+                  <dd className="w-full flex-none text-sm font-medium text-zinc-100">
+                    <span className="italic font-normal text-zinc-100/60">
+                      {org.chapter}
+                    </span>
+                    <span className="mx-1">|</span>
+                    {org.name}
+                  </dd>
+                  <dd className="text-xs text-zinc-400">{org.role}</dd>
+                  <dd
+                    className="ml-auto text-xs text-zinc-500"
+                    aria-label="2019 until Present"
+                  >
+                    <time>{org.duration}</time>
+                  </dd>
+                  <dd className="w-5/6 flex-none text-xs text-zinc-200">
+                    {org.description}
+                  </dd>
+                </dl>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
   );
