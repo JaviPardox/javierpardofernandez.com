fe498f53a2a4db350b4c8c6e8708799ce96ca5f8
JaviPardox <javiyupipa@gmail.com>
2025-02-16 23:17:48 +0100

    Updated introduction

--- frontend/src/components/home/intro/Intro.tsx
+++ frontend/src/components/home/intro/Intro.tsx
@@ -13,13 +13,13 @@ const Intro = () => {
       </div>
       <div className="max-w-2xl" id="home">
         <h1 className="text-4xl sm:text-4.82xl mb-6 mt-7 text-zinc-100 font-inter font-bold tracking-tight leading-[2.5rem] sm:leading-[3.5rem]">
-          Software designer, founder, and amateur astronaut.
+          Software Engineer, Full-Stack Developer, MSc Project Management.
         </h1>
         <p className="mt-6 text-base text-zinc-400 leading-relaxed leading-[1.78rem]">
-          I'm Spencer, a software designer and entrepreneur based in New York
-          City. I'm the founder and CEO of Planetaria, where we develop
-          technologies that empower regular people to explore space on their own
-          terms.
+          I'm Javier, a Software Engineer based in Oslo, Norway. I specialize in software and web development, 
+          with expertise in architecture, testing, and performance optimization. Currently working at Bio-Me, 
+          where I develop and maintain microbiome profiling software solutions while driving innovation and 
+          efficiency.
         </p>
         <SocialIconRow />
       </div>
