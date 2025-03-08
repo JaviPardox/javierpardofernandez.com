38b967a7efecde84a9ea3c2a4e74e9b0ad4ad481
JaviPardox <javiyupipa@gmail.com>
2025-03-08 19:45:59 +0100

    Updated CORS functionality to work with dev and prod

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BACKEND_PORT = os.getenv("BACKEND_PORT", "8000")
 FRONTEND_PORT = os.getenv("FRONTEND_PORT", "3000")
-SERVER_IP = os.getenv("SERVER_IP", "localhost")
+PRIVATE_NETWORK_IP = os.getenv("PRIVATE_NETWORK_IP", "localhost")
+ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
+
+# Define CORS settings based on environment
+if ENVIRONMENT == "production":
+    PRODUCTION_DOMAIN = os.getenv("PRODUCTION_DOMAIN", "yourdomain.com")
+    ALLOWED_ORIGINS = [
+        f"https://{PRODUCTION_DOMAIN}",
+        f"https://www.{PRODUCTION_DOMAIN}",
+        f"http://{PRODUCTION_DOMAIN}",  # Include HTTP for testing or non-HTTPS scenarios
+        f"http://www.{PRODUCTION_DOMAIN}"
+    ]
+    ALLOWED_METHODS = ["GET"]
+    ALLOWED_HEADERS = ["Content-Type"]
+else:
+    ALLOWED_ORIGINS = [
+        f"http://{PRIVATE_NETWORK_IP}:{FRONTEND_PORT}",
+        "http://localhost:3000"
+    ]
+    ALLOWED_METHODS = ["*"]
+    ALLOWED_HEADERS = ["*"]
--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from routes.work_experience import router as work_experience_router
 from routes.images import router as images_router
 from routes.blog import router as blog_router
 from routes.records import router as records_router
-from config import FRONTEND_PORT, SERVER_IP
+from config import ALLOWED_ORIGINS, ALLOWED_METHODS, ALLOWED_HEADERS
 
 app = FastAPI()
 
@@ -16,21 +16,15 @@ app.include_router(blog_router)
 app.include_router(records_router)
 app.mount("/images", StaticFiles(directory="./data/img/cards"), name="images")
 
-
-allowed_origins = [
-    f"http://{SERVER_IP}:{FRONTEND_PORT}",
-    "http://localhost:3000"
-]
-
-# Configure CORS
+# Configure CORS based on environment
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins,
+    allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=ALLOWED_METHODS,
+    allow_headers=ALLOWED_HEADERS,
+    max_age=3600,
 )
-
 
 @app.get("/")
 async def read_root():
